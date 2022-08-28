71ae2d70fb648f50d1e319ab0e26f387281dd05c
sharkovsky <francesco.cremonesi@epfl.ch>
2022-08-28 17:38:18 +0200

    Improvements to main.py
    - add missing line to drop some columns from dataset
    - revert to using pickle
    - introduce argparse

--- docker/main.py
+++ docker/main.py
@@ -1,37 +1,44 @@
-from joblib import load
+from pickle import load
 import pandas as pd
 import numpy as np
+import argparse
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-d', '--data-dir', nargs='?', default='/input')
+parser.add_argument('-m', '--models-dir', nargs='?', default='/models')
+parser.add_argument('-o', '--output-dir', nargs='?', default='/output')
+args = parser.parse_args()
+
 alpha=(
-    pd.read_csv("../training_data_2022-07-21/alpha_diversity/alpha_diversity.csv", 
+    pd.read_csv(args.data_dir + "/alpha_diversity/alpha_diversity.csv", 
                       index_col="specimen")
 )
 species=(
-    pd.read_csv("../training_data_2022-07-21/taxonomy/taxonomy_relabd.species.csv", 
+    pd.read_csv(args.data_dir + "/taxonomy/taxonomy_relabd.species.csv", 
                       index_col="specimen")
 )
 cst=(
-    pd.read_csv("../training_data_2022-07-21/community_state_types/cst_valencia.csv", 
+    pd.read_csv(args.data_dir + "/community_state_types/cst_valencia.csv", 
                       index_col="specimen")
 )
 phylo=(
-    pd.read_csv("../training_data_2022-07-21/phylotypes/phylotype_relabd.1e0.csv", 
+    pd.read_csv(args.data_dir + "/phylotypes/phylotype_relabd.1e0.csv", 
                       index_col="specimen")
 )
 genus=(
-    pd.read_csv("../training_data_2022-07-21/taxonomy/taxonomy_relabd.species.csv", 
+    pd.read_csv(args.data_dir + "/taxonomy/taxonomy_relabd.species.csv", 
                       index_col="specimen")
 )
 #important genus for preterm birth
 genus_preterm=["Lactobacillus","Prevotella"]
 genus=genus[genus_preterm]
 metadata=(
-    pd.read_csv("../training_data_2022-07-21/metadata/metadata.csv", index_col="specimen", na_values="Unknown")
+    pd.read_csv(args.data_dir + "/metadata/metadata.csv", index_col="specimen", na_values="Unknown")
 )
 
 phylo_metadata=pd.merge(phylo, metadata, left_index=True, right_index=True)
@@ -46,18 +53,22 @@ complete_df=(
              left_index=True, right_index=True)
 )
 
+complete_df.drop(["was_preterm", "was_term", "project", "participant_id", "delivery_wk", "was_early_preterm", "collect_wk"], axis=1, inplace=True)
+
 X_train=complete_df.copy()
 
 X_train_species=X_train[species.columns]
 X_train_phylo=X_train[phylo.columns]
 
-filename = 'Models/kmediods_species_final.sav'
-km = load(filename)
+filename = args.models_dir + '/kmediods_species_final.sav'
+with open(filename, 'rb') as f:
+    km = load(f)
 
 X_train["Kmediods_cluster"]=km.predict(X_train_species)
 
-filename = 'Models/kmediods_phylo_final.sav'
-km_phylo = load(filename)
+filename = args.models_dir + '/kmediods_phylo_final.sav'
+with open(filename, 'rb') as f:
+    km_phylo = load(f)
 
 X_train["Kmediods_phylo"]=km_phylo.predict(X_train_phylo)
 
@@ -67,10 +78,12 @@ num_variables = (
     .drop(["Kmediods_cluster","Kmediods_phylo"]).to_list()
 )
 
-filename = 'Models/RandomForest_preterm_final.sav'
-term_pipe_RF = load(filename)
-filename = 'Models/RandomForest_early_preterm_final.sav'
-early_term_pipe_RF = load(filename)
+filename = args.models_dir + '/RandomForest_preterm_final.sav'
+with open(filename, 'rb') as f:
+    term_pipe_RF = load(f)
+filename = args.models_dir + '/RandomForest_early_preterm_final.sav'
+with open(filename, 'rb') as f:
+    early_term_pipe_RF = load(f)
 preds = term_pipe_RF.predict(X_train)
 early_preds = early_term_pipe_RF.predict(X_train)
 
@@ -81,5 +94,5 @@ output.loc[:, 'pred_proba_was_early_preterm'] = early_preds
 output.loc[:, 'pred_was_early_preterm'] = preds > 0.113617
 to_return = output.groupby('participant_id').min()
 
-filename = 'output/predictions.csv'
+filename = args.output_dir + '/predictions.csv'
 to_return.to_csv(filename)
